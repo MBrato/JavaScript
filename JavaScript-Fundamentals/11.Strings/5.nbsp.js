a85e7993daaa220636b6984afc1b6ad5b0d9381f
MBrato <elektra_m@abv.bg>
2017-12-01 09:48:01 +0200

    update

---
+++ JavaScript-Fundamentals/11.Strings/5.nbsp.js
@@ -0,0 +1,32 @@
+/*
+Write a function that replaces non breaking white-spaces in a text with &nbsp.
+*/
+
+let input = [ 'This text contains 4 spaces!!' ];
+
+function nbsp1(args){
+	let str = args[0],
+		result = '',
+		len = str.length,
+		i;
+
+	for(i = 0; i < len; i += 1){
+		if(str[i] ===  ' '){
+			result += '&nbsp';
+		} else {
+			result += str[i];
+		}
+	}
+	return console.log(result);
+}
+
+function nbsp2(args){
+	var str = args[0],
+    	result = str.replace(/\s/g, '&nbsp;');
+
+	return console.log(result);
+
+}
+
+//nbsp1(input);
+nbsp2(input);
